c68961f92da7c909030f29fb68a4f2872a59c998
davidsmenesesc <david86543@hotmail.com>
2023-05-03 15:03:17 -0500

    Modifying

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,9 +1,6 @@
 provider "aws" {
   region = var.region
 }
-resource "aws_eip" "my_eip" {
-  vpc = var.vpc_id
-}
 resource "aws_instance" "public" {
   ami           = "ami-0aa2b7722dc1b5612"
   instance_type = "t2.micro"
