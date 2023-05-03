5822e22c3e56279f5359674ad8cf8bd4ae172de1
davidsmenesesc <david86543@hotmail.com>
2023-05-03 10:08:19 -0500

    First commit

---
+++ Terraform_Devops/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=0.12.6"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"  
+      version = "~> 4.0.0"
+    }
+    vault = {
+      source = "hashicorp/aws"    
+      version = "~> 2.1"
+    }
+  }
+}
