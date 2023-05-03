110f65be5093a3e515cea15dafe969d9add47d15
davidsmenesesc <david86543@hotmail.com>
2023-05-03 15:07:38 -0500

    Modifying

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,24 +1,24 @@
 provider "aws" {
   region = var.region
 }
-# resource "aws_instance" "public" {
-#   ami           = "ami-0aa2b7722dc1b5612"
-#   instance_type = "t2.micro"
-#   key_name      = "NEBo-key"
-#   subnet_id     = var.subnet_ids_pub
-#   vpc_security_group_ids = [ var.pub_sg ]
+resource "aws_instance" "public" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  key_name      = "NEBo-key"
+  subnet_id     = var.subnet_ids_pub
+  vpc_security_group_ids = [ var.pub_sg ]
   
-#   tags = {
-#     Name = "Public Instance"
-#   }
-#   connection {
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     private_key = file("~/.ssh/mykeypair.pem")
-#     host        = self.public_ip
-#   }
-#   depends_on = [ var.pub_sg ]
-# }
+  tags = {
+    Name = "Public Instance"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/mykeypair.pem")
+    host        = self.public_ip
+  }
+  depends_on = [ var.pub_sg ]
+}
 
 resource "aws_instance" "private" {
   ami           = "ami-0aa2b7722dc1b5612"
