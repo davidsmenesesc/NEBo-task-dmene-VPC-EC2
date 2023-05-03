9a6760f0ffd78361aedc8e5a2eca154c83758f82
davidsmenesesc <david86543@hotmail.com>
2023-05-03 14:13:18 -0500

    Modifying

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -3,3 +3,7 @@ variable "region" {
   type = string
   default = "us-east-1"
 }
+variable "subnet_ids" {
+  description = "List of IDs for the subnets to launch instances in"
+  type        = list(string)
+}
