665013904b47bbc980d468994a346cbd2bf2f3c9
davidsmenesesc <david86543@hotmail.com>
2023-05-03 11:16:25 -0500

    Configuration change

--- Terraform_Devops/main.tf
+++ Terraform_Devops/main.tf
@@ -1,11 +1,20 @@
+# terraform {
+#     cloud {
+#         organization = "davidsmenesesc"
+#         workspaces {
+#         name = ""
+#         }
+#     }
+# }
 terraform {
-    cloud {
-        organization = "davidsmenesesc"
-        workspaces {
-        name = "NEBo-task-dmene-VPC-EC2"
-        }
+  backend "remote" {
+    organization = "davidsmenesesc"
+    workspaces {
+      name = "NEBo-task-dmene-VPC-EC2"
     }
+  }
 }
+
 provider "aws"{
     region =  var.region
 }
