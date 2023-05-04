7fde920d7ec6b944bc325a89677678a8543e23e3
davidsmenesesc <david86543@hotmail.com>
2023-05-04 08:43:30 -0500

    Added tunnel SSH

--- Networking/main.tf
+++ Networking/main.tf
@@ -12,6 +12,7 @@ resource "aws_subnet" "snet-public" {
   vpc_id = aws_vpc.vnet-nebo.id
   cidr_block = "10.0.0.0/17"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "snet-public"
   }
