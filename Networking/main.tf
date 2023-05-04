8a30fbb06e6ac402b90c9a146abd05cb4b7152c6
davidsmenesesc <david86543@hotmail.com>
2023-05-04 09:27:42 -0500

    Added tunnel SSH

--- Networking/main.tf
+++ Networking/main.tf
@@ -55,7 +55,7 @@ resource "aws_route_table_association" "snet-public" {
 }
 resource "aws_route_table_association" "snet-private" {
   subnet_id = aws_subnet.snet-private.id
-  route_table_id =  aws_route_table.pub-rt.id
+  route_table_id =  aws_route_table.public-rt.id
   depends_on = [ aws_subnet.snet-private ]
 }
 # Define the internet gateway
